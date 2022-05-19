c5445947b050e998c60093930f5f13d7d33c93b7
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-05-19 09:05:30 -0300

    update

--- apps/devsoutinho_app/lib/main.dart
+++ apps/devsoutinho_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:devsoutinho_app/screens/home/home_screen.dart';
 import 'package:devsoutinho_ui/devsoutinho_ui.dart';
+import 'utils/web/url_strategy/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
