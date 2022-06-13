7b18f672750494e322a660b12f4060bd4e15e73a
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-12 21:19:17 -0300

    update

--- _apps/devsoutinho_app/lib/main.dart
+++ _apps/devsoutinho_app/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     router.addRoutes(routes);
 
     return MaterialApp(
-      title: 'DevSoutinho',
+      title: 'DevSoutinho Labs',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
